fb196531a630b2760ae6bf8431ff9ab5b4e5acc4
Clay Wells <cwells@cwells.org>
2017-02-06 18:01:58 -0500

    code clean-up from code-inspect and pylint.

--- drupal-static-review.py
+++ drupal-static-review.py
@@ -10,11 +10,11 @@ import sys
 import os
 import yaml
 
-# Local file variables (global)
+# ######### LOCAL VARIABLES #########
 infiles = []
 
 
-# Local functions
+# ######### LOCAL FUNCTIONS #########
 
 # Static report
 def find_search_strings():
@@ -37,48 +37,60 @@ def find_search_strings():
 # Menu report
 def find_menu_paths():
     print '[+] finding menu paths..'
+    menupaths = ['TODO']
+    return menupaths
 
 
+# Find all files to be searched
 def find_all_files():
     print '[+] finding all files..'
     for root, dirs, files in os.walk(fullPath):
-        for file in files:
-            if file.endswith(".module") or file.endswith(".inc") or file.endswith(".install") or file.endswith(".php"):
-                print(os.path.join(root, file))
-                infiles.append(os.path.join(root, file))
+        for f in files:
+            if f.endswith(".module") or f.endswith(".inc") or \
+                    f.endswith(".install") or f.endswith(".php"):
+                print(os.path.join(root, f))
+                infiles.append(os.path.join(root, f))
 
 
+# Report header
 def create_staticreport_header():
     report_header = ''
     report_header = 'Static Report for ' + moduleName + '\n'
     report_header += 'Search type: ' + searchType + '\n'
     report_header += '==================================================\n\n'
-    if debugMessages == True:
+    if debugMessages:
         print '\n\n' + report_header
     return report_header
 
 
+# Menu report header
 def create_menureport_header():
     report_header = None
     report_header = 'Menu Report for ' + moduleName + '\n'
     report_header += '==================================================\n\n'
-    if debugMessages == True:
+    if debugMessages:
         print '\n\n' + report_header
     return report_header
 
 
+# These should be read from config file
+'''
 sqlSearch = ["mysql", "query"]
 inputSearch = ["$form_state", "_GET", "_POST", "_REQUEST"]
 # not including t(), st()
-sanitizationSearch = ["check_markup", "check_plain", "check_url", "drupal_attributes",
-                      "drupal_strip_dangerous_protocols", "filter_xss", "format_string", "get_t"]
+sanitizationSearch = ["check_markup", "check_plain", "check_url",
+                      "drupal_attributes", "drupal_strip_dangerous_protocols",
+                      "filter_xss", "format_string", "get_t"]
+'''
 
-# Main section
+
+# ######### MAIN PROGRAM #########
 
 # get arguments
 moduleName = sys.argv[1]
 
-# Allow search for specific types of strings, e.g. sql, or all if len(sys.argv[2]):
+# Allow search for specific types of strings, e.g. sql, or all if
+# len(sys.argv[2]):
 try:
     searchType = sys.argv[2]
 except:
@@ -104,8 +116,9 @@ fullPath = codeDirectory + '/' + moduleName
 # Find all of the files we are interested in searching
 find_all_files()
 
-##############################################################################
+# #############
 # Static Report
+# #############
 
 # Find all the strings we are interested in
 stringReport = find_search_strings()
@@ -119,14 +132,15 @@ fs.write(staticHeader)
 fs.write(stringReport)
 fs.close()
 
-##############################################################################
+# ###########
 # Menu Report
-#
+# ###########
+# TODO: still deciding on whether we should break this out to another script
+#       or not.
 # Find all of the menu paths and flag those that are admin
 menuReport = find_menu_paths()
 
 
-##############################################################################
+##############
 # Input Report
-#
-# TODO: still deciding on whether we should break this out or not.
+# ############
